f24129ff21c26ce7d4217c2c05127c99801d0eb4
github3107 <ram.pendem@gmail.com>
2019-08-04 10:02:55 -0400

    changed reqs

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
-import os
-
 from flask import Flask
-
 app = Flask(__name__)
 
-
-
 @app.route("/")
-
-def main():
-
-    return "Welcome!"
-
-
-
-@app.route('/how are you')
-
 def hello():
-
-    return 'I am good, how about you?'
-
-
+    return "Hello World!"
 
 if __name__ == "__main__":
-
-    app.run(host="0.0.0.0", port=8080)
+    app.run()
